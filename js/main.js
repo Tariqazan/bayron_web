b84aab4b80b235521d5eb8e03fdb1820f078d088
Tariqazan <45086258+Tariqazan@users.noreply.github.com>
2022-12-08 01:11:57 +0600

    animation updated

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-$("#navigation").hide();
-$("#info_1").hide();
-$("#info_2").hide();
-$("#info_3").hide();
-$("#company_info").hide();
-$("#company").hide();
-$("#contact").hide();
-$("#footer").hide();
-
+// loading screen js
 var loadingScreen;
 
 function loader() {
@@ -17,69 +9,147 @@ function showPage() {
     document.getElementById("loading-screen").style.display = "none";
     document.getElementById("main-body").style.display = "block";
 }
-let element = 0
-$(window).bind('mousewheel DOMMouseScroll', function (event) {
-    var lastScrollTop = 0;
-    var st = $(this).scrollTop();
-    if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-        // scroll up
-        element = element - 1
+
+// scrolling animation
+
+document.addEventListener('wheel', scrollPage);
+
+function scrollPage(e) {
+    var nodesName = ['SECTION'];
+    if (nodesName.includes(e.target.nodeName)) {
+        var next = e.target.nextElementSibling;
+        var prev = e.target.previousElementSibling;
+    } else {
+        var next = e.target.closest(nodesName).nextElementSibling;
+        var prev = e.target.closest(nodesName).previousElementSibling;
     }
-    else {
-        // scroll down
-        element = element + 1
+
+    if (e.deltaY < 0) {
+        e.preventDefault();
+        if (nodesName.includes(prev.nodeName)) {
+            verticalScroll(prev, 1500, 'easeInOutCubic');
+            e.target.closest(nodesName).classList.remove('active')
+            prev.classList.add('active')
+            var prev_id = prev.getAttribute("id")
+
+
+            if (prev_id === 'info_1') {
+                document.getElementsByClassName("bayron-partners-corporation-content-logo")[0].classList.add("img_slide_up")
+                document.getElementsByClassName("bayron-partners-corporation-content")[0].classList.add("slide_down_text")
+            }
+            else if (document.getElementsByClassName("bayron-partners-corporation-content-logo")[0].classList.contains("img_slide_up") && document.getElementsByClassName("bayron-partners-corporation-content")[0].classList.contains("slide_down_text")) {
+                document.getElementsByClassName("bayron-partners-corporation-content-logo")[0].classList.remove("img_slide_up")
+                document.getElementsByClassName("bayron-partners-corporation-content")[0].classList.remove("slide_down_text")
+            }
+            else if (prev_id === 'info_2') {
+                document.getElementsByClassName("product-img")[0].classList.add("img_slide_up")
+                document.getElementsByClassName("product-info")[0].classList.add("slide_down_text")
+            }
+            else if (document.getElementsByClassName("product-img")[0].classList.contains("img_slide_up") && document.getElementsByClassName("product-info")[0].classList.contains("slide_down_text")) {
+                document.getElementsByClassName("product-img")[0].classList.remove("img_slide_up")
+                document.getElementsByClassName("product-info")[0].classList.remove("slide_down_text")
+            }
+            else if (prev_id === 'info_3') {
+                document.getElementsByClassName("product-img")[1].classList.add("img_slide_up")
+                document.getElementsByClassName("product-info")[1].classList.add("slide_down_text")
+            }
+            else if (document.getElementsByClassName("product-img")[1].classList.contains("img_slide_up") && document.getElementsByClassName("product-info")[1].classList.contains("slide_down_text")) {
+                document.getElementsByClassName("product-img")[1].classList.remove("img_slide_up")
+                document.getElementsByClassName("product-info")[1].classList.remove("slide_down_text")
+            }
+        }
+    } else if (e.deltaY > 0) {
+        e.preventDefault();
+        if (nodesName.includes(next.nodeName)) {
+            verticalScroll(next, 1500, 'easeInOutQuad', runAfter);
+            e.target.closest(nodesName).classList.remove('active');
+            next.classList.add('active');
+
+            var next_id = next.getAttribute("id")
+
+            if (next_id === 'info_1') {
+                document.getElementsByClassName("bayron-partners-corporation-content-logo")[0].classList.add("img_slide_up")
+                document.getElementsByClassName("bayron-partners-corporation-content")[0].classList.add("slide_down_text")
+            }
+            else if (document.getElementsByClassName("bayron-partners-corporation-content-logo")[0].classList.contains("img_slide_up") && document.getElementsByClassName("bayron-partners-corporation-content")[0].classList.contains("slide_down_text")) {
+                document.getElementsByClassName("bayron-partners-corporation-content-logo")[0].classList.remove("img_slide_up")
+                document.getElementsByClassName("bayron-partners-corporation-content")[0].classList.remove("slide_down_text")
+            }
+            else if (next_id === 'info_2') {
+                document.getElementsByClassName("product-img")[0].classList.add("img_slide_up")
+                document.getElementsByClassName("product-info")[0].classList.add("slide_down_text")
+            }
+            else if (document.getElementsByClassName("product-img")[0].classList.contains("img_slide_up") && document.getElementsByClassName("product-info")[0].classList.contains("slide_down_text")) {
+                document.getElementsByClassName("product-img")[0].classList.remove("img_slide_up")
+                document.getElementsByClassName("product-info")[0].classList.remove("slide_down_text")
+            }
+            else if (next_id === 'info_3') {
+                document.getElementsByClassName("product-img")[1].classList.add("img_slide_up")
+                document.getElementsByClassName("product-info")[1].classList.add("slide_down_text")
+            }
+            else if (document.getElementsByClassName("product-img")[1].classList.contains("img_slide_up") && document.getElementsByClassName("product-info")[1].classList.contains("slide_down_text")) {
+                document.getElementsByClassName("product-img")[1].classList.remove("img_slide_up")
+                document.getElementsByClassName("product-info")[1].classList.remove("slide_down_text")
+            }
+        }
+    } else {
+        return false;
     }
-    if (element === 0) {
-        $("#banner").fadeIn("fast", "linear");
-        $("#navigation").fadeOut();
-        $("#info_1").fadeOut();
-        $("#info_2").fadeOut();
-        $("#info_3").fadeOut();
-        $("#company_info").fadeOut();
-        $("#company").fadeOut();
-        $("#contact").fadeOut();
-        $("#footer").fadeOut();
+}
+
+function runAfter() {
+}
+
+function verticalScroll(destination) {
+    var duration = arguments.length <= 1 || arguments[1] === undefined ? 200 : arguments[1];
+    var easing = arguments.length <= 2 || arguments[2] === undefined ? 'linear' : arguments[2];
+    var callback = arguments[3];
+    var easings = {
+        easeInOutQuad: function easeInOutQuad(t) {
+            return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+        },
+        easeInCubic(t) {
+            return t * t * t;
+        },
+        easeInOutCubic(t) {
+            return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+        }
+    };
+
+
+    var start = window.pageYOffset;
+    var startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+    var documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+    var destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+    var destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+
+
+    if ('requestAnimationFrame' in window === false) {
+        window.scroll(0, destinationOffsetToScroll);
+        if (callback) {
+            callback();
+        }
+        return;
     }
-    else if (element === 1) {
-        $("#navigation").fadeIn();
-        $("#info_1").fadeIn("fast", "linear");
-        $("#banner").fadeOut();
-        $("#info_2").fadeOut();
-        $("#info_3").fadeOut();
-        $("#company_info").fadeOut();
-        $("#company").fadeOut();
-        $("#contact").fadeOut();
-        $("#footer").fadeOut();
+
+    function scroll() {
+        var now = 'now' in window.performance ? performance.now() : new Date().getTime();
+        var time = Math.min(1, (now - startTime) / duration);
+        var timeFunction = easings[easing](time);
+        window.scroll(0, Math.ceil(timeFunction * (destinationOffsetToScroll - start) + start));
+
+        if (window.pageYOffset === destinationOffsetToScroll) {
+            if (callback) {
+                callback();
+            }
+            return;
+        }
+
+        requestAnimationFrame(scroll);
     }
-    else if (element === 2) {
-        $("#navigation").fadeIn();
-        $("#info_2").fadeIn();
-        $("#banner").fadeOut();
-        $("#info_1").fadeOut();
-        $("#info_3").fadeOut();
-        $("#company_info").fadeOut();
-        $("#company").fadeOut();
-        $("#contact").fadeOut();
-        $("#footer").fadeOut();
-    }
-    else if (element === 3) {
-        $("#navigation").fadeIn();
-        $("#info_3").fadeIn();
-        $("#banner").fadeOut();
-        $("#info_2").fadeOut();
-        $("#info_1").fadeOut();
-        $("#company_info").fadeOut();
-        $("#company").fadeOut();
-        $("#contact").fadeOut();
-        $("#footer").fadeOut();
-    }
-    else {
-        $("#info_3").fadeOut();
-        $("#company_info").fadeIn()
-        $("#company").fadeIn();
-        $("#contact").fadeIn();
-        $("#footer").fadeIn();
-    }
-    lastScrollTop = st;
-});
+
+    scroll();
+}
+
 
